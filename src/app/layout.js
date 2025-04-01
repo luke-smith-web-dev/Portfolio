d718d074293502f78a53b449acf6d7ef9da673e9
Luke Smith <2lukesmith@gmail.com>
2025-04-01 16:48:30 -0600

    font fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet"
+      ></link>
       <body className={`antialiased`} id="top">
         <div data-scroll-container>{children}</div>
       </body>
