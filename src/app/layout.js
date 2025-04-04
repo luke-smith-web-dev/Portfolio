1d915c2dc3ddf77d2919168131ea5a0758e2fdeb
Luke Smith <2lukesmith@gmail.com>
2025-04-04 10:49:48 -0600

    metadata update

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,39 @@ import "./globals.css";
 export const metadata = {
   title: "Luke Smith",
   description: "Multidisciplinary Designer based in Missoula, MT",
+  ogImage: "/images/og-image.png",
+  twitterImage: "/images/twitter-image.png",
+  openGraph: {
+    title: "Luke Smith",
+    description: "Multidisciplinary Designer based in Missoula, MT",
+    url: "https://luke-smith.design",
+    images: [
+      {
+        url: "/images/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Luke Smith - Multidisciplinary Designer based in Missoula, MT",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@lukesmithdesign",
+    creator: "@lukesmithdesign",
+    images: [
+      {
+        url: "/images/twitter-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Luke Smith - Multidisciplinary Designer based in Missoula, MT",
+      },
+    ],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
