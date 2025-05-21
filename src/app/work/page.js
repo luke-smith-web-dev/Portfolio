d14f8de0424afe0144f0132335f60a21920c22c7
Luke Smith <2lukesmith@gmail.com>
2025-05-20 19:11:33 -0600

    work update

--- src/app/work/page.js
+++ src/app/work/page.js
@@ -8,9 +8,13 @@ export default function Work() {
           <h3 className="font-[600] text-2xl">Pulse AI Creative</h3>
           <p className="opacity-50 text-lg font-[500]">2025</p>
         </div>
-        <div className="flex justify-between mt-2 mb-10">
+        <div className="flex justify-between mt-2">
           <p className="opacity-50 text-lg font-[600]">Tools:</p>
-          <p className="opacity-50 text-lg font-[500]">Figma, Framer</p>
+          <p className="opacity-50 text-md font-[500]">Figma, Framer</p>
+        </div>
+        <div className="flex justify-between mt-2 mb-10">
+          <p className="opacity-50 text-sm font-[600]">Status:</p>
+          <p className="opacity-50 text-sm font-[500]">Work in Progress</p>
         </div>
         <img
           src="/pac1.png"
@@ -42,9 +46,13 @@ export default function Work() {
           <h3 className="font-[600] text-2xl">ORNGE•Keyboard</h3>
           <p className="opacity-50 text-lg font-[500]">2025</p>
         </div>
-        <div className="flex justify-between mt-2 mb-10">
+        <div className="flex justify-between mt-2">
           <p className="opacity-50 text-lg font-[600]">Tools:</p>
-          <p className="opacity-50 text-lg font-[500]">Blender, Twinmotion</p>
+          <p className="opacity-50 text-md font-[500]">Blender, Twinmotion</p>
+        </div>
+        <div className="flex justify-between mt-2 mb-10">
+          <p className="opacity-50 text-sm font-[600]">Status:</p>
+          <p className="opacity-50 text-sm font-[500]">Completed</p>
         </div>
         <img
           src="/keyboard1.jpg"
@@ -66,6 +74,48 @@ export default function Work() {
         />
         <img
           src="/keyboard4.jpg"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+      </div>
+      <div>
+        <div className="flex justify-between mt-20">
+          <h3 className="font-[600] text-2xl">Concrete.Hold</h3>
+          <p className="opacity-50 text-lg font-[500]">2025</p>
+        </div>
+        <div className="flex justify-between mt-2">
+          <p className="opacity-50 text-lg font-[600]">Tools:</p>
+          <p className="opacity-50 text-md font-[500] text-end">
+            Blender, Twinmotion,
+            <br />
+            Physical Tools, 3D Printing
+          </p>
+        </div>
+        <div className="flex justify-between mt-2 mb-10">
+          <p className="opacity-50 text-sm font-[600]">Status:</p>
+          <p className="opacity-50 text-sm font-[500]">Work in Progress</p>
+        </div>
+        <img
+          src="/Penholder.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+        <img
+          src="/Penholder1.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+        <img
+          src="/Penholder2.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+        <img
+          src="/Penholder3.png"
           alt=""
           style={{ width: "100%", height: "auto" }}
           className="rounded-lg mt-5"
