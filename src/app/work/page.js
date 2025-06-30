87eee378fe224276a51820bd5887fe661614baf9
Luke Smith <2lukesmith@gmail.com>
2025-06-30 14:23:57 -0600

    opal m1 finished!

--- src/app/work/page.js
+++ src/app/work/page.js
@@ -5,6 +5,70 @@ export default function Work() {
     <div className="mx-5 xl:mx-[30rem] mt-20">
       <div>
         <div className="flex justify-between mt-10">
+          <h3 className="font-[600] text-2xl">Opal M1</h3>
+          <p className="opacity-50 text-lg font-[500]">2025</p>
+        </div>
+        <div className="flex justify-between mt-2">
+          <p className="opacity-50 text-lg font-[600]">Tools:</p>
+          <p className="opacity-50 text-md font-[500]">
+            Pen+Paper, 3d Printing, Twinmotion
+          </p>
+        </div>
+        <div className="flex justify-between mt-2 mb-10">
+          <p className="opacity-50 text-sm font-[600]">Status:</p>
+          <p className="opacity-50 text-sm font-[500]">Completed</p>
+        </div>
+        <img
+          src="/render4.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+        <img
+          src="/render11.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+        <img
+          src="/render12.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+        <img
+          src="/render3.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+        <img
+          src="/render14.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+        <img
+          src="/render13.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+        <img
+          src="/sketchesprototypes1.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+        <img
+          src="/opalm1drawing.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+      </div>
+      <div>
+        <div className="flex justify-between mt-10">
           <h3 className="font-[600] text-2xl">Pulse AI Creative</h3>
           <p className="opacity-50 text-lg font-[500]">2025</p>
         </div>
