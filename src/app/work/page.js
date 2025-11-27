34f1c51848ee5004e41bb79479040e19b5118bbe
luke-smith-web-dev <2lukesmith@gmail.com>
2025-11-27 10:15:31 -0700

    new project

--- src/app/work/page.js
+++ src/app/work/page.js
@@ -5,6 +5,58 @@ export default function Work() {
     <div className="mx-5 xl:mx-[30rem] mt-20">
       <div>
         <div className="flex justify-between mt-10">
+          <h3 className="font-[600] text-2xl">Vyken GE-1</h3>
+          <p className="opacity-50 text-lg font-[500]">2025</p>
+        </div>
+        <div className="flex justify-between mt-2">
+          <p className="opacity-50 text-lg font-[600]">Tools:</p>
+          <p className="opacity-50 text-md font-[500]">
+            Pen+Paper, 3d Printing, Blender
+          </p>
+        </div>
+        <div className="flex justify-between mt-2 mb-10">
+          <p className="opacity-50 text-sm font-[600]">Status:</p>
+          <p className="opacity-50 text-sm font-[500]">Completed</p>
+        </div>
+        <img
+          src="/lamp11.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+        <img
+          src="/lamp15-less-blur.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+        <img
+          src="/lamp4.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+        <img
+          src="/lamp9.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+        <img
+          src="/lamp13.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+        <img
+          src="/lamp3.png"
+          alt=""
+          style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mt-5"
+        />
+      </div>
+      <div>
+        <div className="flex justify-between mt-10">
           <h3 className="font-[600] text-2xl">Opal M1</h3>
           <p className="opacity-50 text-lg font-[500]">2025</p>
         </div>
